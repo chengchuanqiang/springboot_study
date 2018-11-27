1fa38ce5613140e25a7cebbb32ce1772c6f67cf0
chengchuanqiang <1625320316@qq.com>
2018-11-27 11:00:55 +0800

    二叉搜索树页面实现

---
+++ src/main/java/com/ccq/springbootkafka/algorithm/tree/bst/BTView.java
@@ -0,0 +1,56 @@
+package com.ccq.springbootkafka.algorithm.tree.bst;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+/********************************
+ ***
+ ***@Author chengchuanqiang
+ ***@Date 2018/11/26 19:47
+ ***@Version 1.0.0
+ ********************************/
+public class BTView extends Pane {
+
+    private BST<Integer> tree = new BST<>();
+
+    private double radius = 15;
+    private double vGap = 50;
+
+    BTView(BST<Integer> tree) {
+        this.tree = tree;
+        setStatus("Tree is empty");
+    }
+
+    public void setStatus(String msg) {
+        getChildren().add(new Text(20, 20, msg));
+    }
+
+    public void displayTree() {
+        this.getChildren().clear();
+        if (tree.root != null) {
+            displayTree(tree.root, getWidth() / 2, vGap, getWidth() / 4);
+        }
+    }
+
+    private void displayTree(BST.TreeNode<Integer> root, double x, double y, double hGap) {
+        if (root.left != null) {
+            getChildren().add(new Line(x - hGap, y + vGap, x, y));
+            displayTree(root.left, x - hGap, y + vGap, hGap / 2);
+        }
+
+        if (root.right != null) {
+            getChildren().add(new Line(x + hGap, y + vGap, x, y));
+            displayTree(root.right, x + hGap, y + vGap, hGap / 2);
+        }
+
+        Circle circle = new Circle(x, y, radius);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.BLACK);
+        getChildren().addAll(circle, new Text(x - 4, y + 4, root.element + ""));
+    }
+
+
+}
