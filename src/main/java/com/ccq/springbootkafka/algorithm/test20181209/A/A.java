5b8287d0693b11a09a11be70e5b1b18d4eb97a9f
chengchuanqiang <1625320316@qq.com>
2018-12-09 22:04:59 +0800

    leetcode代码提交

---
+++ src/main/java/com/ccq/springbootkafka/algorithm/test20181209/A/A.java
@@ -0,0 +1,76 @@
+package com.ccq.springbootkafka.algorithm.test20181209.A;
+
+import java.util.Scanner;
+
+/**
+ * @Author: ChengChuanQiang
+ * @Description:
+ * @Date: Created in 2018/12/9 20:38
+ */
+public class A {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()) {
+            int n = input.nextInt();
+            int m = input.nextInt();
+
+            int[][] res = getResult(n, m);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (j == 0) {
+                        System.out.print(res[i][j]);
+                    } else {
+                        System.out.print(" " + res[i][j]);
+                    }
+
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    private static int[][] getResult(int n, int m) {
+        int[][] res = new int[n][m];
+
+        int i = 0;
+        int j = 0;
+
+        int sum = n * m;
+        int num = 1;
+        while (num <= sum) {
+            // 向右
+            while (j < m && res[i][j] == 0) {
+                res[i][j++] = num++;
+            }
+            j--;
+            i++;
+
+            // 向下
+            while (i < n && res[i][j] == 0) {
+                res[i++][j] = num++;
+            }
+            i--;
+            j--;
+
+            // 向左
+            while (j >= 0 && res[i][j] == 0) {
+                res[i][j--] = num++;
+            }
+            j++;
+            i--;
+
+            // 向上
+            while (i >= 0 && res[i][j] == 0) {
+                res[i--][j] = num++;
+            }
+            i++;
+            j++;
+        }
+        return res;
+    }
+
+
+}
