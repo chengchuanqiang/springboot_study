74d2e96baf072be145e650926bcbb07383820e52
chengchuanqiang <chengchuanqiang@renrenche.com>
2019-05-20 10:23:40 +0800

    输出具体的方案数

--- src/main/java/com/ccq/springbootkafka/algorithm/acwing/fourth/LeetCode279.java
+++ src/main/java/com/ccq/springbootkafka/algorithm/acwing/fourth/LeetCode279.java
@@ -10,6 +10,7 @@ public class LeetCode279 {
     public static void main(String[] args) {
         LeetCode279 test = new LeetCode279();
         System.out.println(test.numSquares(13));
+        System.out.println(test.numSquares2(52));
     }
 
     /**
@@ -28,6 +29,31 @@ public class LeetCode279 {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
+        return dp[n];
+    }
+
+    public int numSquares2(int n) {
+        int[] dp = new int[n + 1];
+        int[] pre = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 0; j * j <= i; j++) {
+                if (dp[i] > dp[i - j * j] + 1) {
+                    dp[i] = dp[i - j * j] + 1;
+                    pre[i] = j * j;
+                }
+
+            }
+        }
+        int index = n;
+        int p = pre[index];
+        while (p != 0) {
+            System.out.print(p + " ");
+            index = index - p;
+            p = pre[index];
+        }
+        System.out.println();
         return dp[n];
     }
 
