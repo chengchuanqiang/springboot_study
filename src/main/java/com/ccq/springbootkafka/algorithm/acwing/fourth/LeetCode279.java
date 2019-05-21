1d88e9e89d962e5b989ff18eaef9367a0ecd74a6
chengchuanqiang <chengchuanqiang@renrenche.com>
2019-05-21 18:33:50 +0800

    输出具体的方案数

--- src/main/java/com/ccq/springbootkafka/algorithm/acwing/fourth/LeetCode279.java
+++ src/main/java/com/ccq/springbootkafka/algorithm/acwing/fourth/LeetCode279.java
@@ -1,5 +1,10 @@
 package com.ccq.springbootkafka.algorithm.acwing.fourth;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Description: https://leetcode.com/problems/perfect-squares/
  * @Author: ChengChuanQiang
@@ -10,7 +15,8 @@ public class LeetCode279 {
     public static void main(String[] args) {
         LeetCode279 test = new LeetCode279();
         System.out.println(test.numSquares(13));
-        System.out.println(test.numSquares2(52));
+        System.out.println(test.numSquares2(5001));
+        System.out.println(test.numSquares3(5001));
     }
 
     /**
@@ -55,6 +61,58 @@ public class LeetCode279 {
         }
         System.out.println();
         return dp[n];
+    }
+
+    public int numSquares3(int n) {
+        int[] dp = new int[n + 1];
+        Map<Integer, List<Integer>> preMap = new HashMap<>();
+        for (int i = 0; i <= n; i++) {
+            preMap.put(i, new ArrayList<>());
+        }
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 0; j * j <= i; j++) {
+                if (dp[i] > dp[i - j * j] + 1) {
+                    dp[i] = dp[i - j * j] + 1;
+                    List<Integer> pre = new ArrayList<>();
+                    pre.add(j * j);
+                    preMap.put(i, pre);
+                } else if (dp[i] == dp[i - j * j] + 1) {
+                    preMap.get(i).add(j * j);
+                }
+            }
+        }
+        dfs(n, new ArrayList<>(), preMap, preMap.get(n));
+        System.out.println(res);
+        return dp[n];
+    }
+
+    List<List<Integer>> res = new ArrayList<>();
+
+    /**
+     * 输出所有的方案，从小到大
+     *
+     * @param n      n
+     * @param curr   当前值
+     * @param preMap 前缀Map
+     * @param pres   前缀list
+     */
+    private void dfs(int n, List<Integer> curr, Map<Integer, List<Integer>> preMap, List<Integer> pres) {
+
+        if (n == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (Integer pre : pres) {
+            if (curr.size() > 0 && curr.get(curr.size() - 1) > pre) {
+                continue;
+            }
+            curr.add(pre);
+            dfs(n - pre, curr, preMap, preMap.get(n - pre));
+            curr.remove(curr.size() - 1);
+        }
     }
 
 }
