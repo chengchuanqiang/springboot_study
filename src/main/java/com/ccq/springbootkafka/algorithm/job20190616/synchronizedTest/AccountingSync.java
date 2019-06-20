13ccd0a1243231387c2c6357f3c6b4d9ea59dfda
chengchuanqiang <chengchuanqiang@renrenche.com>
2019-06-20 16:14:08 +0800

    Integer和String

---
+++ src/main/java/com/ccq/springbootkafka/algorithm/job20190616/synchronizedTest/AccountingSync.java
@@ -0,0 +1,40 @@
+package com.ccq.springbootkafka.algorithm.job20190616.synchronizedTest;
+
+/********************************
+ *** synchronized 作用于实例方法
+ ***@Author chengchuanqiang
+ ***@Date 2019/6/19 17:03
+ ***@Version 1.0.0
+ ********************************/
+public class AccountingSync implements Runnable {
+
+    /**
+     * 共享资源、临界资源
+     */
+    static int num = 0;
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 1000000; j++) {
+            increase();
+        }
+    }
+
+    private synchronized void increase() {
+        num++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AccountingSync accountingSync = new AccountingSync();
+        Thread t1 = new Thread(accountingSync);
+        Thread t2 = new Thread(accountingSync);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(num);
+    }
+}
